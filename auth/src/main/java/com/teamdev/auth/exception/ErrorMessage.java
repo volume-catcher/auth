0ec41b875ff1f9ce1e5e1f9f77eccc258293ac35
이승현 <dltmdgus741@gmail.com>
2022-11-04 00:12:35 +0900

    feat: 장치 번호 불일치 시 인증 실패 처리

--- auth/src/main/java/com/teamdev/auth/exception/ErrorMessage.java
+++ auth/src/main/java/com/teamdev/auth/exception/ErrorMessage.java
@@ -10,8 +10,11 @@ public enum ErrorMessage {
     CONTRACT_FORBIDDEN("유효하지 않은 계약입니다"),
     AUTH_FORBIDDEN("유효하지 않은 인증입니다"),
     EXCEED_NUM_OF_AUTH_AVAILABLE_FORBIDDEN("인증 가능한 횟수를 초과하였습니다"),
+
     CONTRACT_NOT_FOUND("존재하지 않는 계약입니다"),
     AUTH_NOT_FOUND("존재하지 않는 인증입니다"),
+
+    DEVICE_FORBIDDEN("잘못된 장치입니다"),
     ;
 
     private final String message;
